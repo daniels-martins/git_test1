5b13affee28f06f86afcc642b50cfed0eebc0add
Martins <ttc@TTCs-MacBook-Air.local>
2025-07-02 16:59:30 +0100

    learnt how to use web.php route file

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/contact', function () {
+    return 'contact page';
+});
+
+
+
+Route::get('/about', function () {
+    return 'about page';
 });
